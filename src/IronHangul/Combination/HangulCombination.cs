3478dbaebb209c1c325b12c2d4a02cb2bcb74ce4
dowol <dowol.dev@gmail.com>
2026-02-02 20:32:51 +0900

    Create utils for hangul combination

---
+++ src/IronHangul/Combination/HangulCombination.cs
@@ -0,0 +1,24 @@
+using IronHangul.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronHangul.Combination
+{
+    /// <summary>
+    /// 한글 조합 또는 분해 유틸리티를 제공합니다. 
+    /// </summary>
+    public static class HangulCombination
+    {
+        /// <summary>
+        /// 한글 음절 텍스트에서 초성을 추출합니다.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static string ExtractChoseong(this string s, HangulExtractionMode mode = HangulExtractionMode.CompatibilityJamo)
+        {
+            
+        }
+    }
+}
