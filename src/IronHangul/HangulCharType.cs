493c09b23abb17e6b1e80c171a664f0a1ee08d48
dowol <dowol.dev@gmail.com>
2026-02-02 20:33:08 +0900

    create utils for hangul ctype

---
+++ src/IronHangul/HangulCharType.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronHangul
+{
+    /// <summary>
+    /// 현대 한글 관련 유틸리티 메소드를 제공합니다.
+    /// </summary>
+    public static class HangulCharType
+    {
+        /// <summary>
+        /// 해당 문자가 한글인지 확인합니다.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangul(this char c)
+        {
+            return IsHangulSyllable(c) || IsHangulCompatibilityJamo(c) || IsHangulConjoiningJamo(c);
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 자음인지 확인합니다. (유니코드 한글 호환성 자모(Hangul Compatibility Jamo) 영역에서만 적용)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulConsonant(this char c)
+        {
+            return c >= 0x3131 || c <= 0x314e;
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 모음인지 확인합니다. (유니코드 한글 호환성 자모(Hangul Compatibility Jamo) 영역에서만 적용)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulVowel(this char c)
+        {
+            return c >= 0x314f || c <= 0x3163;
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 조합형 초성인지 확인합니다. (유니코드 한글 자모(Hangul Jamo) 영역에서만 적용)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulChoseong(this char c)
+        {
+            return c >= 0x1100 && c <= 0x1112;
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 조합형 중성인지 확인합니다. (유니코드 한글 자모(Hangul Jamo) 영역에서만 적용)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulJungseong(this char c)
+        {
+            return c >= 0x1161 && c <= 0x1175;
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 조합형 종성(받침)인지 확인합니다. (유니코드 한글 자모(Hangul Jamo) 영역에서만 적용)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulJongseong(this char c)
+        {
+            return c >= 0x11a8 && c <= 0x11c2;
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 자모(낱자)인지 확인합니다.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulJamo(this char c)
+        {
+            return IsHangulConjoiningJamo(c) || IsHangulCompatibilityJamo(c);
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 조합형 자모(낱자)인지 확인합니다. (유니코드 한글 자모(Hangul Jamo) 영역인지 확인)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulConjoiningJamo(this char c)
+        {
+            return IsHangulChoseong(c) || IsHangulJungseong(c) || IsHangulJongseong(c);
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 일반 자모(낱자)인지 확인합니다. (유니코드 한글 호환 자모(Hangul Compatibility Jamo) 영역인지 확인)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulCompatibilityJamo(this char c)
+        {
+            return IsHangulConsonant(c) || IsHangulVowel(c);
+        }
+
+        /// <summary>
+        /// 해당 문자가 한글 음절 조합인지 확인합니다. (유니코드 한글 음절(Hangul Syllable) 영역인지 확인)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsHangulSyllable(this char c)
+        {
+            return c >= 0xac00 && c <= 0xd7af;
+        }
+
+    }
+}
